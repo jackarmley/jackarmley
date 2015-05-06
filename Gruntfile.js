6d05a72c50458331616770a32fa52af863af40bb
jackarmley <jack_armley@yahoo.co.uk>
2015-05-06 20:59:37 +0800

    gruntfile changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,266 +27,27 @@ module.exports = function (grunt) {
         }
     });
 
-    // Common tasks
     ext.configure({
         watch: {
-            options: {
-                atBegin: true
-            },
-            startup: {
-                files: [],
-                tasks: ['default'],
-                spawn: true
-            },
-            js: {
-                files: [
-                    '<%= assets.scripts %>/**/*.js',
-                    '!<%= assets.scripts %>/**/*min.js',
-                ],
-                tasks: [
-                    'jshint',
-                    'requirejs'
-                ],
-                spawn: true
-            },
-            css: {
-                files: '<%= assets.sass %>/**/*.scss',
-                tasks: [
-                    'sass:development',
-                    'modernizr:dist',
-                    'autoprefixer',
-                    'cmq'
-                ],
-                spawn: true
-            },
-            wintersmith: {
-                files: [
-                    'templates/**/*',
-                    'mixins/**/*',
-                    'contents/**/*',
-                    'config.json'
-                ],
-                tasks: [
-                    'wintersmith:build'
-                ],
-                spawn: true
-            }
+          sourceFiles: {
+            files: ['themes/**','scaffolds/**','scripts/**','source/**'],
+            tasks: 'shell:hexoGen'
+          },
         }
     });
 
-    // Stylesheets
-    ext.configure({
-        sass: {
-            development: {
-                options: {
-                    style: 'expanded',
-                    debugInfo: false,
-                    sourcemap: true,
-                    noCache: true
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: '<%= assets.sass %>',
-                        src: '*.scss',
-                        dest: '<%= assets.css %>',
-                        ext: '.css',
-                        extDot: 'last'
-                    },
-                ]
-            },
-            production: {
-                options: {
-                    style: 'compressed',
-                    debugInfo: false,
-                    sourcemap: false,
-                    noCache: true
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: '<%= assets.sass %>',
-                        src: '*.scss',
-                        dest: '<%= assets.css %>',
-                        ext: '.css',
-                        extDot: 'last'
-                    },
-                ]
-            }
-        },
-        autoprefixer: {
-            options: {
-                browsers: ['last 3 versions', '> 1%', 'ie 8', 'ie 7']
-            },
-            dist: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: '<%= assets.css %>',
-                        src: '*.css',
-                        dest: '<%= assets.css %>',
-                        ext: '.css',
-                        extDot: 'last'
-                    },
-                ]
-            }
-        },
-        cmq: {
-            combine: {
-                options: {
-                    log: false
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: '<%= assets.css %>',
-                        src: '*.css',
-                        dest: '<%= assets.css %>',
-                        ext: '.css',
-                        extDot: 'last'
-                    },
-                ]
-            }
-        },
-        cssmin: {
-            combine: {
-                options: {
-                    banner: '<%= banner %>'
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: '<%= assets.css %>',
-                        src: '*.css',
-                        dest: '<%= assets.css %>',
-                        ext: '.css',
-                        extDot: 'last'
-                    },
-                ]
-            }
-        }
-    });
-
-    // Javascript
-    ext.configure({
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                '<%= assets.scripts %>/{,*/}*.js',
-                '!<%= assets.scripts %>/{,*/}*.min.js'
-            ]
-        },
-        requirejs: {
-            compile: {
-                options: {
-                    name: 'app',
-                    baseUrl: '<%= assets.scripts %>',
-                    mainConfigFile: '<%= assets.scripts %>/app.js',
-                    out: '<%= assets.scripts %>/app.min.js'
-                }
-            }
-        },
-        modernizr: {
-            dist: {
-                devFile: 'remote',
-                outputFile: '<%= assets.scripts %>/../libs/modernizr/modernizr.min.js',
-                extra: {
-                    'shiv': true,
-                    'load': false,
-                    'cssclasses': true
-                },
-                uglify: true,
-                parseFiles: true,
-                files: {
-                    src: [
-                        '<%= assets.css %>/*.css',
-                        '<%= assets.scripts %>/app.min.js'
-                    ]
-                }
-            }
-        }
-    });
-
-    // Image minification
-    ext.configure({
-        imagemin: {
-            production: {
-                options: {
-                    optimizationLevel: 7
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: '<%= assets.images %>',
-                        src: ['**/*.{png,jpg,gif}'],
-                        dest: '<%= assets.images %>'
-                    }
-                ]
-            }
-        }
-    });
-
-    // Wintersmith
-    ext.configure({
-        wintersmith: {
-            build: {
-                options: {
-                    action: 'build',
-                    config: './config.json'
-                }
-            }
-        },
-        connect: {
-            build: {
-                options: {
-                    port: 3009,
-                    base: 'build',
-                    hostname:'*',
-                    keepalive: true
-                }
-            }
-        }
-    });
-
-    // Deploy
     ext.configure({
         shell: {
-            rsync: {
-                command: "rsync --update -av -e 'ssh' --progress --remove-sent-files ./build/ jackarmley16@jackarmley.com:jackarmley.com"
-            },
-            dryrun: {
-                command: "rsync --update -av -e 'ssh' --progress --dry-run --remove-sent-files ./build/ jackarmley16@jackarmley.com:jackarmley.com"
-            } 
+          hexoGen: {
+              command: 'hexo generate'
+          }
         }
     });
 
-    // Build for development purposes with linting
+    // Default task.
     ext.registerTask('default', [
-        'sass:development',
-        'autoprefixer',
-        //'jshint',
-        //'requirejs',
-        'modernizr:dist',
-        'wintersmith:build'
+        'watch'
     ]);
-
-    // Server build
-    ext.registerTask('server', [
-        'sass:production',
-        'autoprefixer',
-        'cmq:combine',
-        'cssmin',
-        'requirejs',
-        'modernizr:dist',
-        'imagemin:production',
-        'wintersmith:build'
-    ]);
-
-    // Wintersmith preview
-    ext.registerTask('preview', [
-        'connect'
-    ]);
-
 
     // Load grunt configuration
     ext.initConfig(grunt);
